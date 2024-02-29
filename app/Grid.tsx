be74f201458a70048af7a86e94f7df2ae4d52b42
Alan Shaw <alan.shaw@protocol.ai>
2024-02-29 12:03:24 +0000

    feat: add background colour and logo

--- app/Grid.tsx
+++ app/Grid.tsx
@@ -2,17 +2,19 @@ import { Emojis } from './lib'
 
 export const cellSize = 32
 
+const Logo = () => <svg width={cellSize/2} viewBox="0 0 27.2 27.18" xmlns="http://www.w3.org/2000/svg"><path d="M13.6 27.18A13.59 13.59 0 1127.2 13.6a13.61 13.61 0 01-13.6 13.58zM13.6 2a11.59 11.59 0 1011.6 11.6A11.62 11.62 0 0013.6 2z" fill="currentColor"></path><path d="M12.82 9.9v2.53h1.6V9.9l2.09 1.21.77-1.21-2.16-1.32 2.16-1.32-.77-1.21-2.09 1.21V4.73h-1.6v2.53l-2-1.21L10 7.26l2.2 1.32L10 9.9l.78 1.21zM18 17.79v2.52h1.56v-2.52L21.63 19l.78-1.2-2.16-1.33 2.16-1.28-.78-1.19-2.08 1.2v-2.58H18v2.56L15.9 14l-.77 1.2 2.16 1.32-2.16 1.33.77 1.15zM8.13 17.79v2.52h1.56v-2.52L11.82 19l.77-1.2-2.16-1.33 2.12-1.28-.73-1.24-2.13 1.23v-2.56H8.13v2.56L6.05 14l-.78 1.2 2.16 1.3-2.16 1.33.78 1.17z" fill="currentColor"></path></svg>
+
 export function Grid ({ emojis }: { emojis: Emojis }) {
   const gridSize = emojis.length
   const rowStyle = { display: 'flex' }
-  const cellStyle = { display: 'flex', justifyContent: 'center', alignItems: 'center', width: cellSize, height: cellSize, fontSize: cellSize }
-  const indexCellStyle = { ...cellStyle, fontSize: 14, fontFamily: 'sans-serif', paddingTop: 2 }
+  const cellStyle = { display: 'flex', justifyContent: 'center', alignItems: 'center', width: cellSize, height: cellSize, fontSize: cellSize, color: 'black' }
+  const indexCellStyle = { ...cellStyle, fontSize: 14, fontFamily: 'sans-serif', color: 'black', paddingTop: 2 }
 
   const rows = []
   for (let i = 0; i < gridSize + 1; i++) {
     const items = []
     if (i === 0) {
-      items.push(<div style={cellStyle}></div>)
+      items.push(<div style={cellStyle}><Logo /></div>)
       for (let j = 1; j < gridSize + 1; j++) {
         items.push(<div style={indexCellStyle}>{j}</div>)
       }
@@ -29,7 +31,7 @@ export function Grid ({ emojis }: { emojis: Emojis }) {
   const height = width
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width, height }}>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width, height, backgroundColor: 'white' }}>
       {rows}
     </div>
   )
